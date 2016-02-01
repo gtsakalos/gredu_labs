c0a7545ec7b497bd4c6f31989214b5acadaf0c5a
Georgios Tsakalos <ozmax.planet@gmail.com>
2016-02-01 17:40:11 +0200

    update equipment service

--- app/src/DBService/EquipmentService.php
+++ app/src/DBService/EquipmentService.php
@@ -7,6 +7,8 @@ use R;
 
 class EquipmentService
 {
+
+    // Item Category Actions
     
     public function __construct($school_service)
     {
@@ -25,6 +27,17 @@ class EquipmentService
         return $categories;
     }
 
+    public function getItemCategoryByName($name){
+        $cat = R::findOne('itemcategory', 'name = ?', [$name]);
+        return $cat;
+    }
+
+    public function getItemCategoryById($id){
+        $cat = R::load('itemcategory', $id);
+        return $cat;
+    }
+
+    //Software Category Actions
 
     public function createSoftwareCategory(Array $data){
         $soft_category = R::dispense('softwarecategory');
@@ -40,4 +53,41 @@ class EquipmentService
         return $categories;
     }
 
+    public function getSoftwareCategoryByName($name){
+        $cat = R::findOne('softwarecategory', 'name = ?', [$name]);
+        return $cat;
+    }
+
+    public function getSoftwareCategoryById($id){
+        $cat = R::load('softwarecategory', $id);
+        return $cat;
+    }
+
+    //Existing Item Actions
+
+    public function createItem(Array $data){
+        required = ['location', 'category', 'description', 'qty', 'lab', 'purchasedate' ];
+        foreach ($required as $value){
+            $item = R::dispense('existingitem');
+            if (array_key_exists($value, $data))
+            {
+                $item[$value] = data[$value];
+            }
+            else
+            {
+                return -1
+            }
+            if (array_key_exists('comments', $data))
+            {
+                $item['comments'] = $data['comments'];
+            }
+            else
+            {
+                $item['comments'] = '';
+            }
+            R::store($item);
+        }
+    
+    }
+    
 }
