0d181483536487a6db7bf95557144484a94430cf
Georgios Tsakalos <ozmax.planet@gmail.com>
2016-02-02 15:08:50 +0200

    update lab service
    make teacher not required

--- app/src/DBService/LabService.php
+++ app/src/DBService/LabService.php
@@ -18,18 +18,14 @@ class LabService
     public function createLab(Array $data)
     {
         $lab = R::dispense('lab');
-        $required = ['name', 'school', 'teacher', 'area'];
-
+        $required = ['name', 'school', 'area'];
         foreach ($required as $value){
             if (array_key_exists($value, $data)){
                 if ($value == 'school')
                 {
                     $school_id = $data[$value];
                 }
-                else if ($value == 'teacher')
-                {
-                    $teacher_id = $data[$value];
-                }
+                
                 else
                 {
                 $lab[$value] = $data[$value];
@@ -39,6 +35,10 @@ class LabService
             {
                 return -1;
             }
+        }
+        if array_key_exists('teacher', $data)
+        {
+            $teacher_id = $data['teacher'];
         }
         
         $school = $this->school_service->getSchool($school_id);
